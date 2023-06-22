315c6b1b50cc566453e85e27873ca44b944edda2
Jaime Ramirez <jaime.ramirez@redhat.com>
2023-06-22 11:59:22 +0200

    fix

--- hello-node/index.js
+++ hello-node/index.js
@@ -9,7 +9,7 @@ server.get("/", async () => {
 })
 
 server
-  .listen({ port: 3000 })
+  .listen({ port: 3000, host: "0.0.0.0" })
   .catch(err => {
     server.log.error(err);
     process.exit(1);
