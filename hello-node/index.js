51a380eaf40fd418e8bbd888c701299568d73650
Jaime Ramirez <jaime.ramirez@redhat.com>
2023-06-22 11:51:16 +0200

    add example

---
+++ hello-node/index.js
@@ -0,0 +1,18 @@
+// Require the framework and instantiate it
+const Fastify = require("fastify");
+const process = require("node:process");
+
+const server = Fastify({ logger: true });
+
+server.get("/", async () => {
+  return { hello: 'world' }
+})
+
+server
+  .listen({ port: 3000 })
+  .catch(err => {
+    server.log.error(err);
+    process.exit(1);
+  });
+
+console.log(`This process is pid ${process.pid}`);
